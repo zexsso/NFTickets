06eb5f28642a18264a1fd875b1ce4aabc398f9b8
RubenChet <rubenchetboun@gmail.com>
2023-05-15 16:14:54 +0200

    cleaned-vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,6 @@
 <script setup>
-	import TheWelcome from "../components/TheWelcome.vue"
 </script>
 
 <template>
-	<div class="text-5xl font-extralight">
-		<span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"> Hello world </span>
-	</div>
-	<div class="card flex justify-content-center">
-		<Button label="Check" severity="success" icon="pi pi-check" />
-	</div>
+	<p>bonjour</p>
 </template>
