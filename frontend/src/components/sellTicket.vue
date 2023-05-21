d20fff015fcb1fe953bdb76442f704c3da38e8fe
raurenpro <raurenpro@gmail.com>
2023-05-21 19:35:41 +0200

    ajout vente ticket

---
+++ frontend/src/components/sellTicket.vue
@@ -0,0 +1,27 @@
+<template lang="">
+	<div class="flex justify-center mt-12">
+		<form @submit.prevent="sell" class="flex flex-col px-4 space-y-8">
+			<span class="p-float-label">
+				<InputText v-model="user" inputId="user" />
+				<label for="user">Nom d'utilisateur</label>
+			</span>
+			<span class="p-float-label">
+				<Password v-model="password" inputId="password" :feedback="false" />
+				<label for="password">Mot de passe</label>
+			</span>
+
+			<Divider />
+
+			<Button label="Vendre" type="submit" icon="pi pi-user-plus" class="mt-4 text-white bg-indigo-600 hover:bg-indigo-700 border border-transparent" />
+		</form>
+	</div>
+</template>
+
+<script setup>
+	import { ref } from "vue"
+	import { useToast } from "primevue/usetoast"
+
+	const toast = useToast()
+
+	const sell = () => {}
+</script>
--- frontend/src/views/MarketplaceView.vue
+++ frontend/src/views/MarketplaceView.vue
@@ -3,15 +3,23 @@
 		<div>
 			<NavbarVue />
 		</div>
-		<div class="flex h-full">
-			<DataGridTicketVue />
+		<div class="relative h-full">
+			<Button @click="visibleSell = true" class="absolute top-0 right-0 z-10 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded border-none">Vendre ticket</Button>
+			<DataGridTicketVue class="z-0" />
 		</div>
 	</div>
+
+	<Dialog header="Vente" v-model:visible="visibleSell" :modal="true" :style="{ width: '20vw' }">
+		<SellTicketVue />
+	</Dialog>
 </template>
 
 <script setup>
 	import { ref } from "vue"
 
 	import NavbarVue from "../components/navbar.vue"
-    import DataGridTicketVue from "../components/dataGridTicket.vue"
+	import DataGridTicketVue from "../components/dataGridTicket.vue"
+    import SellTicketVue from "../components/sellTicket.vue"
+
+    const visibleSell = ref(false)
 </script>
